f1ef3023e1a1a5235df982a2341e03cd977584b0
Lukazes <lucasprivado99@gmail.com>
2024-08-27 15:54:45 -0300

    atualizando o projeto

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Olá, mundo!")
+let nomeHeroi = "Lucas"
+let xpHeroi = 8500
+const notificacao = "Notificação: "
+
+if(xpHeroi >= 8001){
+    console.log(notificacao + "O herói " + nomeHeroi + " agora é de nível ascendente!")
+    console.log(notificacao + "Ele tem " + xpHeroi + " pontos de xp!")
+} else if (xpHeroi < 8001) {
+    console.log(nomeHeroi + " ainda não é um herói ascendente!")
+} else {
+    console.log(nomeHeroi + " ainda não é um herói!")
+}
